85bdac108e768bf61ce19b655cc8cb76a4f6bead
RahmounFares2001 <faresrahm40@gmail.com>
2024-02-08 12:24:39 +0100

    com

--- src/app/components/home/footer/Footer.jsx
+++ src/app/components/home/footer/Footer.jsx
@@ -9,17 +9,16 @@ import { FaGooglePlus } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 
 
-// 
 export default function Footer() {
   return (
     <section style={{backgroundColor: "#010101"}}
                 className='py-20 flex flex-col gap-10'> 
         <div className='flex justify-center gap-3 sm:gap-10'>
-            <a href=""><FaTwitter className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
-            <a href=""><FaFacebook className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700'/></a>
-            <a href=""><FaLinkedin className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
-            <a href=""><FaGooglePlus className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
-            <a href=""><FaYoutube className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
+            <a href="https://twitter.com/KYO_Conseil"><FaTwitter className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
+            <a href="https://www.facebook.com/KYOconseil/"><FaFacebook className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700'/></a>
+            <a href="https://www.linkedin.com/company/kyo-conseil"><FaLinkedin className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
+            <a href="https://workspaceupdates.googleblog.com/2023/04/new-community-features-for-google-chat-and-an-update-currents%20.html"><FaGooglePlus className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
+            <a href="https://www.youtube.com/channel/UCH0lycJmdz74axY9b1KKVFA"><FaYoutube className='w-10 h-10 sm:w-14 sm:h-14 text-gray-200 hover:text-rose-700' /></a>
         </div>
         
         <h1 className='text-gray-300 text-center text-sm sm:text-xl'>2024 © KYO Conseil. All rights reserved.</h1>
--- src/app/sign/page.jsx
+++ src/app/sign/page.jsx
@@ -2,9 +2,12 @@
 
 import React, { useState } from 'react'
 
-// material ui
-import GoogleIcon from '@mui/icons-material/Google';
-import FacebookIcon from '@mui/icons-material/Facebook';
+// icons
+import { FaGoogle } from "react-icons/fa6";
+import { FaFacebookSquare } from "react-icons/fa";
+
+// css
+import "./sign.css";
 
 
 export default function Sign() {
@@ -38,21 +41,31 @@ export default function Sign() {
 
         {/* sign in  */}
         {/* sign in form */}
-        <div className='flex justify-center items-center shadow-lg shadow-gray-500 w-4/5 lg:w-4/5 max-h-min rounded-xl '>
-            <div className={`${signInForm} w-1/2 signInForm`} >
-                <form action="" className='flex flex-col justify-center items-center mx-8 py-20 gap-5'>
+        <div className='flex justify-center items-center shadow-lg shadow-rose-700/50 w-11/12 sm:w-4/5 max-h-min rounded-xl
+                    text-gray-300'
+                        style={{backgroundColor: '#0d0d0f'}}>
+            
+            <div className='absolute top-3 flex justify-center items-center gap-5 cursor-pointer'>
+                <img src="./images/home/logo.jpg" alt="logo" 
+                    className=' w-10 h-10'/>
+                <h1 className='text-xl text-rose-700 font-bold'>KYO CONSEIL</h1>
+            </div>
+            
+
+            <div className={`${signInForm} w-full lg:w-1/2 fadeIn`} >
+                <form action="" className='flex flex-col justify-center items-center mx-4 sm:mx-8 py-20 gap-5'>
                     <h1 className='text-4xl font-bold w-80 text-center'>Sign In</h1>
                     <div className='flex gap-4'>
-                        <GoogleIcon style={{cursor: "pointer"}} />
-                        <FacebookIcon style={{color : "blue", cursor: "pointer"}} />
+                        <FaGoogle className='text-blue-800 hover:text-rose-700 w-8 h-8' />
+                        <FaFacebookSquare className='text-blue-800 hover:text-rose-700 w-8 h-8'/>
                     </div>
-                    <h1 className='text-sm text-gray-700 w-80 text-center'>or use your account</h1>
-                    <div className='flex flex-col gap-4 justify-center items-center'>
-                        <input type="email" placeholder='Email' className='bg-gray-300/30 px-3 py-2 lg:w-64 sm:w-80 w-64 text-sm' />
-                        <input type="password" placeholder='Password' className='bg-gray-300/30 px-3 py-2 lg:w-64 sm:w-80 w-64 text-sm' />
+                    <h1 className='text-sm text-gray-400 w-80 text-center'>or use your account</h1>
+                    <div className='flex flex-col gap-4 justify-center items-center w-full '>
+                        <input type="email" placeholder='Email' className='bg-gray-300/30 px-3 py-2 lg:w-72 md:w-3/6 sm:w-4/6 w-full text-sm' />
+                        <input type="password" placeholder='Password' className='bg-gray-300/30 px-3 py-2 lg:w-72 md:w-3/6 sm:w-4/6 w-full text-sm' />
                     </div>
-                    <h1 className='text-gray-700 w-80 text-center'>Forgot your password?</h1>
-                    <button className='font-bold text-sm  bg-blue-950 py-3 px-12 lg:w-3/5 sm:w-4/5 w-52 rounded-3xl text-white'>SIGN IN</button>
+                    <h1 className='text-gray-400 w-80 text-center cursor-pointer hover:text-rose-700'>Forgot your password?</h1>
+                    <button className='font-bold text-sm  bg-blue-950 py-3 px-12 lg:w-3/5 md:w-2/5 sm:w-3/5 w-52 rounded-3xl text-white hover:bg-rose-700'>SIGN IN</button>
                     <h1 onClick={signInButton} className='cursor-pointer lg:hidden w-72 text-center'>Or Sign Up</h1>
                 </form>
             </div>
@@ -61,35 +74,35 @@ export default function Sign() {
                 <div className="flex flex-col justify-center items-center px-10 py-6 gap-7 text-center" >
                     <h1 className='text-4xl font-bold'>Hello, Client!</h1>
                     <h1>Enter your personal details and start journey with us</h1>
-                    <button onClick={signInButton} className='border-solid border-2 border-blue-950 rounded-3xl py-2 px-12 text-sm'>SIGN UP</button>
+                    <button onClick={signInButton} className='border-solid border-2 border-blue-950 rounded-3xl py-2 px-12 text-sm hover:border-rose-700'>SIGN UP</button>
                 </div>
             </div>
 
             
             {/* sign up */}
-            <div className={`${signUp} w-1/2 signInForm`}>
+            <div className={`${signUp} w-full lg:w-1/2 signInForm`}>
                 <div className="flex flex-col justify-center items-center px-10 py-6 gap-7 text-center" >
                     <h1 className='text-4xl font-bold'>Hello, Client!</h1>
                     <h1>Enter your personal details and start journey with us</h1>
-                    <button onClick={signUpButton} className='border-solid border-2 border-blue-950 rounded-3xl py-2 px-12 text-sm'>SIGN IN</button>
+                    <button onClick={signUpButton} className='border-solid border-2 border-blue-950 rounded-3xl py-2 px-12 text-sm hover:border-rose-700'>SIGN IN</button>
                 </div>
             </div>
 
             {/* sign up form */}
-            <div className={`${signUpForm} w-1/2 signUpForm`} >
-                <form action="" className='flex flex-col justify-center items-center  mx-8 py-20 gap-5'>
+            <div className={`${signUpForm} w-full lg:w-1/2 fadeIn`} >
+                <form action="" className='flex flex-col justify-center items-center  mx-4 sm:mx-8 py-20 gap-5'>
                     <h1 className='text-4xl font-bold w-80 text-center'>Sign Up</h1>
                     <div className='flex gap-4'>
-                        <GoogleIcon style={{cursor: "pointer"}} />
-                        <FacebookIcon style={{color : "blue", cursor: "pointer"}} />
+                        <FaGoogle className='text-blue-800 hover:text-rose-700 w-8 h-8' />
+                        <FaFacebookSquare className='text-blue-800 hover:text-rose-700 w-8 h-8'/>
                     </div>
-                    <h1 className='text-sm text-gray-700 w-80 text-center' >or use your account</h1>
-                    <div className='flex flex-col gap-4 justify-center items-center'>
-                        <input type="text" placeholder='Username' className='bg-gray-300/30 px-3 py-2 lg:w-64 sm:w-80 w-64 text-sm' />
-                        <input type="email" placeholder='Email' className='bg-gray-300/30 px-3 py-2 lg:w-64 sm:w-80 w-64 text-sm' />
-                        <input type="password" placeholder='Password' className='bg-gray-300/30 px-3 py-2 lg:w-64 sm:w-80 w-64 text-sm' />
+                    <h1 className='text-sm text-gray-400 w-80 text-center' >or create your account</h1>
+                    <div className='flex flex-col gap-4 justify-center items-center w-full'>
+                        <input type="text" placeholder='Username' className='bg-gray-300/30 px-3 py-2 lg:w-72 md:w-3/6 sm:w-4/6 w-full text-sm' />
+                        <input type="email" placeholder='Email' className='bg-gray-300/30 px-3 py-2 lg:w-72 md:w-3/6 sm:w-4/6 w-full text-sm' />
+                        <input type="password" placeholder='Password' className='bg-gray-300/30 px-3 py-2 lg:w-72 md:w-3/6 sm:w-4/6 w-full text-sm' />
                     </div>
-                    <button className='font-bold text-sm  bg-blue-950 py-3 px-12 lg:w-3/5 sm:w-4/5 w-52 rounded-3xl text-white'>SIGN UP</button>
+                    <button className='font-bold text-sm  bg-blue-950 py-3 px-12 lg:w-3/5 md:w-2/5 sm:w-3/5 w-52 rounded-3xl text-white hover:bg-rose-700'>SIGN UP</button>
                     <h1 onClick={signUpButton} className='cursor-pointer lg:hidden w-72 text-center'>Or Sign In</h1>
                 </form>
             </div>
